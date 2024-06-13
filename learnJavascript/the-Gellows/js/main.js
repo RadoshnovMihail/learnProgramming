113085a1b792d6eb5b80aa849e6d4b048a47dcad
Mihail Radoshnov <tenchu047@gmail.com>
2024-06-13 12:48:32 +0300

    +

--- learnJavascript/the-Gellows/js/main.js
+++ learnJavascript/the-Gellows/js/main.js
@@ -13,30 +13,113 @@ let words = [
   "пасифика",
   "дэрил",
 ];
-let word = words[Math.floor(Math.random() * words.length)];
 
-let answerArray = [];
-for (let i = 0; i < word.length; i++) {
-  answerArray[i] = "_";
-}
+// Выбор случайного слова из списка
+const pickWord = function (words) {
+  return words[Math.floor(Math.random() * words.length)];
+};
 
-let remainingLetters = word.length;
+const setupAnswerArray = function () {
+  let wordToGuess = pickWord(words);
+  let answerArray = new Array(wordToGuess.length).fill("-");
+  let remainingLetters = wordToGuess.length;
+  let attempts = 0;
+  return {
+    wordToGuess: wordToGuess,
+    answerArray: answerArray,
+    remainingLetters: remainingLetters,
+    attempts: attempts,
+  };
+};
 
-while (remainingLetters > 0) {
-  alert(answerArray.join(" "));
-  let guess = prompt("Угадай букву или нажми Отмена для выхода из игры.");
-  if (guess === null) {
-    break;
-  } else if (guess.length !== 1) {
-    alert("Пожалуйста, введи только одну букву.");
-  } else {
-    for (let j = 0; j < word.length; j++) {
-      if (word[j] === guess.toLowerCase()) {
-        answerArray[j] = guess.toLowerCase();
-        remainingLetters--;
-      }
+// Обновление состояния игры после догадки пользователя
+const updateGameState = function (guess, word, answerArray) {
+  let correctGuess = false;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === guess) {
+      answerArray[i] = guess;
+      correctGuess = true;
     }
   }
+  return correctGuess;
+};
+
+// Основная функция игры
+function startGame() {
+  let game = setupAnswerArray();
+  alert("Добро пожаловать в игру 'Угадай слово'!");
+
+  while (game.remainingLetters > 0) {
+    alert("Отгаданное слово: " + game.answerArray.join(" "));
+    let userGuess = prompt(
+      "Введите букву или нажмите 'Отмена' для выхода из игры."
+    );
+
+    if (userGuess == null) {
+      alert("Игра окончена. До новых встреч!");
+      return;
+    } else if (userGuess.length !== 1) {
+      alert("Пожалуйста, введите только одну букву.");
+    } else {
+      let correctGuess = updateGameState(
+        userGuess,
+        game.wordToGuess,
+        game.answerArray
+      );
+      if (correctGuess) {
+        game.remainingLetters--;
+      }
+      game.attempts++;
+    }
+  if (game.remainingLetters === 0) {
+    alert(
+      "Поздравляем! Вы угадали слово '" +
+        game.wordToGuess +
+        "' за " +
+        game.attempts +
+        " попыток."
+    );
+  }
+  }
 }
-alert(answerArray.join(" "));
-alert("Отлично! Было загадано имя " + word);
+startGame();
+
+// let word = words[Math.floor(Math.random() * words.length)];
+// let pickRandomWord = function (words){
+//   return words[Math.floor(Math.random() * words.length)];
+// };
+
+// pickRandomWord(randomWord);
+
+// let answerArray = [];
+// for (let i = 0; i < word.length; i++) {
+//   answerArray[i] = "_";
+// }
+
+// let remainingLetters = word.length;
+// let count = 10;
+
+// while (remainingLetters > 0) {
+//   alert(answerArray.join(" "));
+//   let guess = prompt("Угадай букву или нажми Отмена для выхода из игры.");
+//   if (guess === null) {
+//     break;
+//   } else if (guess.length !== 1) {
+//     alert("Пожалуйста, введи только одну букву.");
+//   } else {
+//     for (let j = 0; j < word.length; j++) {
+//       if (word[j] === guess.toLowerCase()) {
+//         answerArray[j] = guess.toLowerCase();
+//         remainingLetters--;
+//       }
+//     }
+//   }
+//   if (count > 0) {
+//     alert(`попыток осталось ${count}`);
+//     count--;
+//   } else {
+//     break;
+//   }
+// }
+// alert(answerArray.join(" "));
+// alert("Отлично! Было загадано имя " + word);
