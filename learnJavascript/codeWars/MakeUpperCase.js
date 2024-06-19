c9d89c4261610f540789516fc017a6a690be1a2a
Mihail Radoshnov <tenchu047@gmail.com>
2024-06-19 10:50:21 +0300

    +

---
+++ learnJavascript/codeWars/MakeUpperCase.js
@@ -0,0 +1,5 @@
+// Напишите функцию, которая преобразует входную строку в верхний регистр.
+
+function makeUpperCase(str) {
+   return str.toUpperCase();
+  }
---
+++ learnJavascript/codeWars/Where Are My Glasses.js
@@ -0,0 +1,28 @@
+// Oh no! I've lost my glasses, but paradoxically, I need glasses to find my glasses!
+
+// Please help me out by showing me the index in the list which contains my glasses. They look like two capital Os, with at least one dash in between!
+
+// This means that both O--O and O------------O are valid glasses, but not O----#--O for example!
+// Search thoroughly, you might find my glasses in places such as 'dustO-Odust'
+// Examples
+// ["phone", "O-O", "coins", "keys"] ➞ 1
+
+// ["OO", "wallet", "O##O", "O----O"] ➞ 3
+
+// ["O--#--O", "dustO---Odust", "more dust"] ➞ 1
+// Notes
+// All lists will include one valid pair of glasses because I swear I dropped them around here somewhere ...
+// All elements in the list are strings.
+
+
+
+function findGlasses(arr){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].match(/O-+O/)) {
+            return i;
+        }
+    }
+    return -1; 
+}
+
+  console.log(findGlasses(["phone", "O-O", "coins", "keys"]))
