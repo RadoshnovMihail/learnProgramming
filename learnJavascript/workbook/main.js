103ac87d0d437c6be841b86912b857041ee08b44
Mihail Radoshnov <tenchu047@gmail.com>
2024-06-25 15:22:16 +0300

    +

--- learnJavascript/workbook/main.js
+++ learnJavascript/workbook/main.js
@@ -1,92 +1,83 @@
-// const header = document.querySelector('h1');
-// let topPos = 0;
-// let leftPos = 0;
-// let step = 50;
-// let direction = 'right';
-
-// function moveHeader() {
-//     if (direction === 'right') {
-//         leftPos += step;
-//         if (leftPos >= 200) {
-//             leftPos = 200;
-//             direction = 'down';
-//         }
-//     } else if (direction === 'down') {
-//         topPos += step;
-//         if (topPos >= 200) {
-//             topPos = 200;
-//             direction = 'left';
-//         }
-//     } else if (direction === 'left') {
-//         leftPos -= step;
-//         if (leftPos <= 0) {
-//             leftPos = 0;
-//             direction = 'up';
-//         }
-//     } else if (direction === 'up') {
-//         topPos -= step;
-//         if (topPos <= 0) {
-//             topPos = 0;
-//             direction = 'right';
-//         }
-//     }
-
-//     header.style.top = topPos + 'px';
-//     header.style.left = leftPos + 'px';
+// function sayHi () {
+//   alert('Hello');
 // }
 
-// setInterval(moveHeader, 1000); // Интервал в миллисекундах (здесь 1000 мс = 1 секунда)
+// window.onload = () => {
+//  document.getElementById('button').onclick = sayHi;
+// };
 
+// window.onload = () => {
+//    document.getElementById('button').onclick = () => alert('Hello');
+//    document.getElementById('button-2').onclick = sayHi_2;
+//    document.getElementById('button-3').onclick = sayHi_3;
+// }
 
-const header = document.querySelector('h1');
-let topPos = 0;
-let leftPos = 0;
-let step = 50;
-let direction = "right";
+// window.onload = () => {
+//   document.getElementById('button').onclick = func;
+// };
 
-function moveHeader() {
-  if (direction === "right") {
-    leftPos += 50;
-    if (leftPos >= 200) {
-      leftPos = 200;
-      direction = "down";
-    }
-  }
-  if (direction === "down") {
-    topPos += 50;
-    if (topPos >= 200) {
-      topPos = 200;
-      direction = "left";
-    }
-  }
-  if (direction === "left") {
-    leftPos -= 50;
-    if (leftPos <= 0) {
-      leftPos = 0;
-      direction = "up";
-    }
-  }
-  if (direction === "up") {
-    topPos -= 50;
-    if (topPos <= 0) {
-      topPos = 0;
-      direction = "right";
-    }
-  }
-  header.style.top = topPos + "px";
-  header.style.left = leftPos + "px";
-}
+// function func(){
+//   document.getElementById('button-2').onclick = sayHi_2;
+// }
 
-function clickCount(){
-    let count = 0;
-    while(count <= 800){
-        count++
+// window.onload = () =>{
+//   document.getElementById('button').onclick = sayHi;
+//   func();
+// }
 
-    }
-    }
+// window.addEventListener('load', start);
+// window.addEventListener('load', func);
+
+// window.addEventListener('load', start);
+// window.addEventListener('load', function(){
+
+// });
+
+// window.addEventListener('load', start);
 
 
 
+// window.onload = () => {
+  //   document.getElementById("button").onclick = () => alert("Hello");
+  //   document.getElementById("button-2").onclick = sayHi_2;
+  //   document.getElementById("button-3").onclick = sayHi_3;
+  // };
+  
+  
+//   function sayHi() {
+//     alert("Hello");
+//   }
+//   function sayHi_2() {
+//     alert("Hello 2");
+//   }
+//   function sayHi_3() {
+//     alert("Hello 3");
+//   }
 
+//   function func (){
+//     document.getElementById('button-2').addEventListener('click', sayHi_2);
+//   }
+
+
+// window.addEventListener("load", function () {
+//   document.getElementById('button').addEventListener('click', sayHi);
+//  func();
+// });
+
+// window.addEventListener('load', function(){
+//   let w = screen.width;
+//   let h = screen.height;
+//   document.getElementById('si').innerHTML = w + ' x ' + h;
+// });
+
+let den = new Date();
+let mes = new Date();
+let god = new Date();
+let mon = '';
+let arr=["января", "февраля", "марта", "апреля", "мая", "июня", "июля", 
+  "августа", "сентября", "октября", "ноября", "декабря"];
+
+
+window.addEventListener('load', tim);
 
 
