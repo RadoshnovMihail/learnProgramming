ddcbdd43a0ad30c2b3b72a830c20bd0032c6623b
Mihail Radoshnov <tenchu047@gmail.com>
2024-06-25 21:14:02 +0300

    +

--- learnJavascript/workbook/main.js
+++ learnJavascript/workbook/main.js
@@ -35,15 +35,12 @@
 
 // window.addEventListener('load', start);
 
-
-
 // window.onload = () => {
-  //   document.getElementById("button").onclick = () => alert("Hello");
-  //   document.getElementById("button-2").onclick = sayHi_2;
-  //   document.getElementById("button-3").onclick = sayHi_3;
-  // };
-  
-  
+//   document.getElementById("button").onclick = () => alert("Hello");
+//   document.getElementById("button-2").onclick = sayHi_2;
+//   document.getElementById("button-3").onclick = sayHi_3;
+// };
+
 //   function sayHi() {
 //     alert("Hello");
 //   }
@@ -58,7 +55,6 @@
 //     document.getElementById('button-2').addEventListener('click', sayHi_2);
 //   }
 
-
 // window.addEventListener("load", function () {
 //   document.getElementById('button').addEventListener('click', sayHi);
 //  func();
@@ -70,14 +66,130 @@
 //   document.getElementById('si').innerHTML = w + ' x ' + h;
 // });
 
-let den = new Date();
-let mes = new Date();
-let god = new Date();
-let mon = '';
-let arr=["января", "февраля", "марта", "апреля", "мая", "июня", "июля", 
-  "августа", "сентября", "октября", "ноября", "декабря"];
+// window.addEventListener("load", tim);
+// function tim() {
+//   let den = new Date();
+//   let mes = new Date();
+//   let mon = "";
+//   let arr = [
+//     "января",
+//     "февраля",
+//     "марта",
+//     "апреля",
+//     "мая",
+//     "июня",
+//     "июля",
+//     " августа",
+//     "сентября",
+//     "октября",
+//     "ноября",
+//     "декабря",
+//   ];
+//   for (let i = 0; i < 12; i++) {
+//     if (mes.getMonth() == i) {
+//       mon = arr[i];
+//       break;
+//     }
+//   }
+//   let god = new Date();
+//   document.getElementById("god").innerHTML =
+//     den.getDate() + " " + mon + " " + god.getFullYear() + "года";
+//   let cha = new Date();
+//   let min = new Date();
+//   let sek = new Date();
+//   let m = min.getMinutes();
+//   let mi = "" + m;
+//   if (mi.length == 1) {
+//     m = "0" + m;
+//   }
+//   let s = sek.getSeconds();
+//   let se = "" + s;
+//   if (se.length == 1) {
+//     s = "0" + s;
+//   }
+// }
+// document.getElementById("min").innerHTML = cha.getHours() + ":" + m + ":" + s;
+// window.setTimeout(tim, 1000);
 
+// let a = new Date();
+// let b = a.getMinutes();
+// console.log(b);
 
-window.addEventListener('load', tim);
+// window.addEventListener("load", function () {
+//   document.getElementById("con").addEventListener("click", function () {
+//     if (
+//       window.confirm("Вы действительно хотите покинуть эту страницу?") == true
+//     ) {
+//       window.location = "http://testjs.ru/";
+//     }
+//     return false;
+//   });
+// });
 
+// window.addEventListener("load", () => {
+// document.getElementById("con").addEventListener("click", () => {
+//   if (
+//     confirm('"Вы действительно хотите покинуть эту страницу?') == true
+//   ) {
+//    window.location = "http://testjs.ru/";
+//   }
+//   return false;
+// });
+// });
+// let i = 1;
 
+// window.addEventListener("load", () => {
+//   document.getElementById("flo").addEventListener("click", tra);
+// });
+
+// function tra() {
+//   if (i == 1) {
+//     document.getElementById("flo").style.transition = "width 1s";
+//     document.getElementById("flo").style.width = "500px";
+//     i = 2;
+//   } else {
+//     document.getElementById("flo").style.transition = "width 1s";
+//     document.getElementById("flo").style.width = "200px";
+//     i = 1;
+//   }
+//   return false;
+// }
+
+// let direction = 1;
+
+// window.addEventListener("load", () => {
+//   document.getElementById("flo").addEventListener("click", tra);
+// });
+
+// function tra() {
+//   if (direction == 1) {
+//     document.getElementById("flo").style.transition = "width 1s";
+//     document.getElementById("flo").style.width = "500px";
+//     direction = 2;
+//   } else {
+//     document.getElementById("flo").style.transition = "width 1s";
+//     document.getElementById("flo").style.width = "200px";
+//     direction = 1;
+//   }
+//   return false;
+// }
+
+// window.addEventListener("load", () => {
+//   document.getElementById("pic").addEventListener("click", (event) => {
+//     if (event.target.tagName == "BUTTON") {
+//       let a = event.target.id;
+//       document.getElementById("im").src = "/images/anim/" + a + ".jpg";
+//       document.getElementById("im").style.border = "1px solid #000000";
+//     }
+//   });
+// });
+
+window.addEventListener("load", function () {
+  document.getElementById("pic").addEventListener("click", function (event) {
+    if (event.target.tagName == "BUTTON") {
+      let getBtnId = event.target.id;
+      document.getElementById("im").src = "/images/anim/" + getBtnId + ".jpg";
+      document.getElementById("im").style.border = "1px solid #000000";
+    }
+  });
+});
--- learnJavascript/workbook/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-let counter = 1;
-
-const printMessage = function (){
-  console.log("You can see on the console" + counter + "sec");
-  counter++;
-};
-
-let intervalId = setInterval(printMessage, 1000);
