efa5a0e747a3bf20d64e3c1e7f4befa97fec0f3a
Mihail Radoshnov <tenchu047@gmail.com>
2024-08-07 11:01:20 +0300

    +

---
+++ learnJavascript/codeWars/Who likes it.js
@@ -0,0 +1,41 @@
+// Вы, вероятно, знакомы с системой "лайк" из Facebook и других страниц. Люди могут "лайкать" записи в блоге, картинки или другие объекты. Мы хотим создать текст, который будет отображаться рядом с таким элементом. Реализуйте функцию, которая принимает массив, содержащий имена людей, которым нравится элемент. Она должна возвращать текст для отображения, как показано в примерах:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+function likes(names) {
+  if (!names || !names.length) {
+    return "no one likes this";
+  }
+  if (names.length === 1) {
+    return `${names[0]} likes this`;
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else if (names.length > 3) {
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
+
+console.log(likes([]));
+console.log(likes(["Peter"]));
+console.log(likes(["Jacob", "Alex"]));
+console.log(likes(["Max", "John", "Mark"]));
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]));
+
+
+
+function likes(names) {
+  names = names || [];
+  switch(names.length){
+    case 0: return 'no one likes this'; break;
+    case 1: return names[0] + ' likes this'; break;
+    case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+    case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+    default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+  }
+}
