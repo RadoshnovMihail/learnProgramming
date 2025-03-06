2e8db74dfcbd4e25d54496846a9ebfe6e0267e72
Mihail Radoshnov <tenchu047@gmail.com>
2025-03-07 02:49:06 +0300

    Получал теги и айди  с помощью javascript

---
+++ learnJavascript/javascript2.0/app/script.js
@@ -0,0 +1,9 @@
+// out.innerHTML = '<i>Hi</i>';
+// out.innerText = '<i>Hi</i>';
+// document.getElementById('out').innerHTML = 777;
+
+let p;
+p = document.getElementById('out');
+p = document.querySelector('#out');
+p.innerHTML = 555;
+console.log(p);
