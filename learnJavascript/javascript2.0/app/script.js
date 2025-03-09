7b58d08d8c2efaa387afcc5a1576ee065765ee99
Mihail Radoshnov <tenchu047@gmail.com>
2025-03-10 00:50:30 +0300

    изучаю событие нажатие кнопок на клавиатуры

--- learnJavascript/javascript2.0/app/script.js
+++ learnJavascript/javascript2.0/app/script.js
@@ -1,24 +1,4 @@
-document.querySelector('.push').onclick = function(){
-   
-    let radio = document.querySelectorAll('.radio-input');
-    // console.log(radio);
-    // // radio[2].hidden = true;
-    // for(let i = 0; i < radio.length; i++){
-    //     if(radio[i].checked){
-    //         console.log(radio[i].value);
-    //     }
-    // }
-    // let p = document.querySelectorAll('p');
-    // // console.log(p);
-    // for(let i = 0; i < p.length; i++){
-    //     p[i].style.color = 'red';
-    // }
-    let option = document.querySelectorAll('#my-select option');
-    // console.log(option);
-    for(let i = 0; i < option.length; i++){
-        if(option[i].selected){
-            console.log(option[i].innerHTML);
-        }
-    }
+document.querySelector('#test').onkeypress = function(e){
+    console.log(e.key);
+    return false;
 }
-
