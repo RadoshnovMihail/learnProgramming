5042f598907e623873f128f1fda0e283a16029ba
Mihail Radoshnov <tenchu047@gmail.com>
2025-03-09 15:58:33 +0300

    +

--- learnJavascript/javascript2.0/app/script.js
+++ learnJavascript/javascript2.0/app/script.js
@@ -1,22 +1,24 @@
-let userNum = document.querySelector('#user-num');
-document.querySelector('#btn').onclick = checkNum;
-let randomNum = Math.round(Math.random() * 2);
-console.log(randomNum);
-
-function checkNum(){
-    let num = userNum.value;
-    num = parseInt(num);
-    console.log(num);
-    if(!isNaN(num)){
-       if(num === randomNum){
-        alert('Вы угадали!!!');
-        location.reload();
-       } else {
-        alert('Пробуй еще!')
-        location.reload();
-       }
-    }else{
-        alert('Введите коректное число');
-        location.reload();
+document.querySelector('.push').onclick = function(){
+   
+    let radio = document.querySelectorAll('.radio-input');
+    // console.log(radio);
+    // // radio[2].hidden = true;
+    // for(let i = 0; i < radio.length; i++){
+    //     if(radio[i].checked){
+    //         console.log(radio[i].value);
+    //     }
+    // }
+    // let p = document.querySelectorAll('p');
+    // // console.log(p);
+    // for(let i = 0; i < p.length; i++){
+    //     p[i].style.color = 'red';
+    // }
+    let option = document.querySelectorAll('#my-select option');
+    // console.log(option);
+    for(let i = 0; i < option.length; i++){
+        if(option[i].selected){
+            console.log(option[i].innerHTML);
+        }
     }
 }
+
